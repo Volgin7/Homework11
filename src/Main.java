3559dc965dc5b8315da36f311b33df827ebc4167
Volgin7 <8612020@gmail.com>
2023-03-15 14:34:18 +0300

    +task2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
 
         task1();
+        task2();
 
     } // end of main
 
     public static void task1() {
         int currentYear = 2021;
+        System.out.println("Задача 1");
         checkLeapYear(currentYear);
+        System.out.println();
     } // end of task
 
     public static void checkLeapYear( int year ) {
@@ -15,6 +20,31 @@ public class Main {
             System.out.println(year + " год — високосный год");
         } else {
             System.out.println(year + " год — невисокосный год");
+        }
+    } // end of checkLeapYear
+
+    public static void task2() {
+        int osType = 1;
+        int clientDeviceYear = 2023;
+        System.out.println("Задача 2");
+        chooseOS(osType,clientDeviceYear);
+        System.out.println();
+    } // end of task
+
+    public static void chooseOS( int os, int deviceYear ) {
+        int currentYear = LocalDate.now().getYear();
+        if (deviceYear < currentYear) {
+            if (os == 0) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        } else {
+            if (os == 0) {
+                System.out.println("Установите обычную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите обычную версию приложения для Android по ссылке");
+            }
         }
     } // end of checkLeapYear
 
