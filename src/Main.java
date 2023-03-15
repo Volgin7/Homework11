6343eb15efe3f3202da2738a07e146a7dbcde26e
Volgin7 <8612020@gmail.com>
2023-03-15 14:13:08 +0300

    +task1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        task1();
 
     } // end of main
+
+    public static void task1() {
+        int currentYear = 2021;
+        checkLeapYear(currentYear);
+    } // end of task
+
+    public static void checkLeapYear( int year ) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + " год — високосный год");
+        } else {
+            System.out.println(year + " год — невисокосный год");
+        }
+    } // end of checkLeapYear
+
 } //end of Main
